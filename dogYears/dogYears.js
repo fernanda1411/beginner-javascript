df67f6d8a70a66ae1a413c7a9997917b268e159b
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-09-03 22:42:36 -0700

    Calculate dog age script

---
+++ dogYears/dogYears.js
@@ -0,0 +1,10 @@
+/* Dog Years
+With your knowledge of math operators and variables, 
+use JavaScript to convert your age into dog years.*/
+
+let myAge = 31;
+let firstYears = 2 * 10.5;
+let laterYears = (myAge - 2) * 4;
+let myAgeInDogYears = firstYears + laterYears;
+
+console.log('My age in dog years is ' + myAgeInDogYears);
